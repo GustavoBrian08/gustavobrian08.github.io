a689875719e7da480b8e328b2ca9b4233f96562c
Gustavo Brian <gustavo-dm2010@hotmail.com>
2022-12-05 15:50:09 -0300

    Some editions

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,60 @@ let n = 1
 if(navegation != null){
     window.addEventListener('scroll', () => {
         let value = window.scrollY
-        if (value >= 200) {
-            n = 9
-        } else if (value <= 150){
+        if (value >= 300) {
+            n = 96
+        } else if (value <= 250){
             n = 0
         } else {
             n += 0.5
         }
-        navegation.style.background = `rgba(0, 0, 0, 0.${n})`
+        navegation.style.background = `rgba(18, 18, 18, 0.${n})`
     })
 }
+
+// TYPING ANIMATION
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Web Developer!", "Game Developer!", "Full-stack!"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+const type = () => {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
 
 // SLIDER SKILLS
 
