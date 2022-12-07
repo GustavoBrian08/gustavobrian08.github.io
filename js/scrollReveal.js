bb10a96bbb8ec40a7b46b39363846e60a5db464e
Gustavo Brian <gustavo-dm2010@hotmail.com>
2022-12-07 12:47:34 -0300

    Scroll Reveal added

---
+++ js/scrollReveal.js
@@ -0,0 +1,36 @@
+// SCROLL REVEAL
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('#navegation', {
+  distance: '150%',
+  origin: 'top',
+  opacity: null,
+  duration: 2000
+});
+
+sr.reveal('#header-presentation-container', { duration: 1500 })
+
+sr.reveal('#technologies', {
+  scale: 0.9,
+  duration: 1500
+})
+
+sr.reveal('#projects-list', {
+  scale: 0.9,
+  duration: 1500
+})
+
+sr.reveal('#connect img', { 
+  scale: 0.6,
+  duration: 1700
+})
+
+sr.reveal('#connect form', {
+  delay: 500,
+  duration: 1000
+})
+
+sr.reveal('#my-contacts-bg', {
+    scale: 0.9,
+    duration: 1800
+  })
